c305ba616122a75d443d14631c8c011ff174d84d
FaithKamau <faith.kamau@student.moringaschool.com>
2023-06-10 14:22:24 +0300

    Completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,77 @@
 import React from "react";
-import blogData from "../data/blog";
+import Header from './Header';
+import About from './About';
+import ArticleList from './ArticleList';
+// import Article from './Article';
 
-console.log(blogData);
 
-function App() {
+const App = () => {
+  const blogName = "Underreacted";
+  const blogAbout = "A blog about learning React";
+  const blogPosts = [
+    {
+      id: 1,
+      title: "Components 101",
+      date: "December 15, 2020",
+      preview: "Setting up the building blocks of your site",
+      minutes: 5,
+    },
+    {
+      id: 2,
+      title: "React Data Flow",
+      date: "December 11, 2020",
+      preview: "Passing props is never passé",
+      minutes: 15,
+    },
+    {
+      id: 3,
+      title: "Function vs Class Components",
+      preview: "React, meet OOJS.",
+      minutes: 47,
+    }
+  ];
+
   return (
-    <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+    <div>
+      <Header name={blogName} />
+      A blog about learning React
+      <About about={blogAbout} />
+      <ArticleList posts={blogPosts} />
     </div>
   );
-}
+};
 
 export default App;
+
+
+
+  
+
+
+// const App = () => {
+//   const blogName = "My Blog";
+//   const blogAbout = "Welcome to my blog! This is a place where I share my thoughts and ideas.";
+//   const blogPosts = [
+//     { id: 1, title: "First Post", date: "June 1, 2023", preview: "This is the preview of the first post." },
+//     { id: 2, title: "Second Post", date: "June 5, 2023", preview: "This is the preview of the second post." },
+//     { id: 3, title: "Third Post", preview: "This is the preview of the third post." }
+//   ];
+
+//   return (
+//     <div>
+//       <Header name={blogName} />
+//       <About about={blogAbout} />
+//       <ArticleList posts={blogPosts} />
+//       {/* Rest of your app content */}
+//     </div>
+//   );
+// };
+
+// export default App;
+
+
+
+
+
+
+  
